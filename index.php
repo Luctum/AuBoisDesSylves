3ff1366364d5914db5c921a330b8fe559cd25ad8
Hugo PALLA <hugo.palla@gmail.com>
2016-11-22 23:10:13 +0100

    Deleting old conf file

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-require __DIR__.'/app/config/db.php';
 require __DIR__.'/app/app.php';
 require __DIR__.'/app/routes.php';
 
